eef74125e081e7bd27d173b07c242517dd253318
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-05-05 17:29:25 +0200

    Added additional comments on training scripts and deleted old code

--- src/train_gender_classifier.py
+++ src/train_gender_classifier.py
@@ -8,37 +8,42 @@ Description: Train gender classification model
 
 from keras.callbacks import CSVLogger, ModelCheckpoint
 from data_loader import DataLoader
-from models import simple_CNN, attention_CNN
+from models import attention_CNN
 from image_generator import ImageGenerator
 from utils import split_data
 
-# Parameters and given variables
+# parameters
 batch_size = 32
-num_epochs = 30
+num_epochs = 100
+training_split = .8
+do_random_crop = False
 num_classes = 2
-training_ratio = .8
 dataset_name = 'imdb'
-do_random_crop = True
 input_shape = (48, 48, 3)
 images_path = '../datasets/imdb_crop/'
 log_file_path = 'log_files/gender_training.log'
-trained_models_path = '../trained_models/gender_models/simple_CNN'
+trained_models_path = '../trained_models/gender_models/attention_CNN'
 
+# data loader
 data_loader = DataLoader(dataset_name)
 ground_truth_data = data_loader.get_data()
-train_keys, val_keys = split_data(ground_truth_data, training_ratio)
-image_generator = ImageGenerator(ground_truth_data, batch_size, input_shape[:2],
+train_keys, val_keys = split_data(ground_truth_data, training_split)
+image_generator = ImageGenerator(ground_truth_data, batch_size,
+                                input_shape[:2],
                                 train_keys, val_keys, None,
                                 path_prefix=images_path,
                                 vertical_flip_probability=0,
                                 do_random_crop=do_random_crop)
 
+# model parameters/compilation
 model = attention_CNN(input_shape, num_classes)
 model.compile(optimizer='adam',
             loss='categorical_crossentropy',
             metrics=['accuracy'])
 model.summary()
 
+# model callbacks
+csv_logger = CSVLogger(log_file_path, append=False)
 model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
 model_checkpoint = ModelCheckpoint(model_names,
                                    monitor='val_loss',
@@ -46,7 +51,7 @@ model_checkpoint = ModelCheckpoint(model_names,
                                    save_best_only=True,
                                    save_weights_only=False)
 
-csv_logger = CSVLogger(log_file_path, append=False)
+# model training
 model.fit_generator(image_generator.flow(mode='train'),
                     steps_per_epoch=int(len(train_keys) / batch_size),
                     epochs=num_epochs, verbose=1,
